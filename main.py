07f84dd1a41b34a36b279e1a2db88b4312e5416a
AmanPriyanshu <amanpriyanshusms2001@gmail.com>
2024-11-09 16:34:53 -0500

    cosine

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from src.walk_and_encode_examples import WalkAndEncode
+from src.code_gen import CodeGen
+import os
+import pandas as pd
 
 if __name__=='__main__':
-    wae = WalkAndEncode(api_key="sk-c3ddQQtQ1_M41pX0s9CY3A")
-    encodings_df = wae.walk("./data/")
-    encodings_df.to_csv("example_mapper_rag_assist.csv", index=False)
+    if not os.path.exists("example_mapper_rag_assist.csv"):
+        wae = WalkAndEncode(api_key="sk-c3ddQQtQ1_M41pX0s9CY3A")
+        encodings_df = wae.walk("./data/")
+        encodings_df.to_csv("example_mapper_rag_assist.csv", index=False)
+    else:
+        encodings_df = pd.read_csv("example_mapper_rag_assist.csv")
+    paths = [i.replace('data/', '') for i in encodings_df['file_path'].tolist()]
+    cg = CodeGen(api_key="sk-c3ddQQtQ1_M41pX0s9CY3A", df=encodings_df)
+    cg.generate("Convolutional Nueral Network visualization", paths)
--- src/code_gen.py
+++ src/code_gen.py
@@ -1,13 +1,16 @@
 from sentence_transformers import SentenceTransformer
-import pandas as pd
+from sentence_transformers.util import cos_sim
 from openai import OpenAI
 import json
+import torch
+import numpy as np
 
 PROXY_ENDPOINT = "https://nova-litellm-proxy.onrender.com/"
 
 class CodeGen:
-    def __init__(self, api_key, model_name=None):
+    def __init__(self, api_key, df, model_name=None):
         self.api_key = api_key
+        self.df = df
         self.client = OpenAI(api_key=self.api_key, base_url=PROXY_ENDPOINT)
         if model_name is None:
             self.model_name = "jinaai/jina-embeddings-v2-base-code"
@@ -36,17 +39,26 @@ class CodeGen:
                 }
             }
         ]
+        self.paths, self.encodings, self.question_encodings = self.df['file_path'].tolist(), np.array([json.loads(i) for i in self.df['encoding'].tolist()]), np.array([json.loads(i) for i in self.df['question_encoding'].tolist()])
+        self.encodings, self.question_encodings = torch.tensor(self.encodings).float(), torch.tensor(self.question_encodings).float()
 
-    def generate(self, aim):
-        prompt = "You are a planning assistant with expertise in visual explanation and in Manim, the Graphical Animation Library. Your goal is to observe this given objective by the user: \"{aim}\". Give me steps / kinds of visuals to plan for, for creating a 30 second clip, essentially you're story boarding with not more than 3-5 stages and return a string describe the events that take place in each of those stages. (Understand the limitations of Manim and be simplistic)"
+    def generate(self, aim, path_contexts):
+        prompt = f"You are a story-boarding assistant with expertise in visual explanation and in Manim, the Graphical Animation Library. Your goal is to visualize the concept given by the user: \"{aim}\". Give simple steps for creating a 30 second clip, essentially you're story boarding with not more than 3-5 stages and return a string describe the events that take place in each of those stages. (Understand the limitations of Manim and be simplistic). Note: Here are some example files of code we have to take inspiration for some of the panels for:\n\n{path_contexts}"
         response = self.client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{"role": "user", "content": prompt}],
+            model="openai/gpt-4o",
+            messages=[{"role": "system", "content": "You are a research visualization tool. Mathemetical representations etc."}, {"role": "user", "content": prompt}],
             functions=self.functions,
             function_call={"name": "manim_story_board"}
         )
-        args = json.loads(response.choices[0].message.function_call.arguments)
-        print(args)
+        story_board = json.loads(response.choices[0].message.function_call.arguments)['story_board']
+        for story in story_board:
+            a = self.model.encode(story)
+            print(a.shape)
+            cosine_similarity_raw = cos_sim(a, self.encodings)
+            cosine_similarity_question = cos_sim(a, self.question_encodings)
+            print(cosine_similarity_question, cosine_similarity_raw)
+            exit()
+
         # prompt = """You are a coding assistant with expertise in Manim, Graphical Animation Library. \n
         #     Here is a full set of Manim documentation:  \n ------- \n  {context} \n ------- \n Answer the user
         #     question based on the above provided documentation. Ensure any code you provide can be executed \n
